54e8a509c1d00cb1a664040114074ef249f6e31f
Maarten-Marx <r0929437@student.thomasmore.be>
2023-11-25 19:38:39 +0100

    Implement refunds

--- WpfCaseStudy/Windows/Pages/OrderLineList.xaml.cs
+++ WpfCaseStudy/Windows/Pages/OrderLineList.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using WpfCaseStudy.Controllers;
 using WpfCaseStudy.Schema;
 
@@ -17,10 +19,33 @@ public partial class OrderLineList
 
     private readonly int _orderId;
 
-    private readonly OrderLineDataController _dc = new();
+    private readonly OrderLineDataController _orderLineDc = new();
+    private readonly ClientDataController _clientDc = new();
 
     private void LoadEntries()
     {
-        OrderLines.ItemsSource = _dc.GetWhere(l => l.OrderId == _orderId);
+        OrderLines.ItemsSource = _orderLineDc.GetWhere(l => l.OrderId == _orderId);
+    }
+
+    private void RefundEntry(OrderLine line, int amount)
+    {
+        _orderLineDc.UpdateWhere(
+            l => l.Id == line.Id,
+            l => l.Refunded += amount
+        );
+        _clientDc.UpdateWhere(
+            c => c.Id == line.Order.ClientId,
+            c => c.Credit += line.Product.ExportPrice * amount
+        );
+        LoadEntries();
+    }
+
+    private void OpenRefund(object sender, MouseButtonEventArgs e)
+    {
+        if ((sender as ListViewItem)?.DataContext is OrderLine line)
+        {
+            var refund = new Refund(line, RefundEntry);
+            refund.ShowDialog();
+        }
     }
 }
---
+++ WpfCaseStudy/Windows/Refund.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WpfCaseStudy.Schema;
+
+namespace WpfCaseStudy.Windows;
+
+public partial class Refund
+{
+    public Refund(OrderLine line, Action<OrderLine, int> onRefund)
+    {
+        InitializeComponent();
+        Product.Text = $"Product: {line.Product.Name}";
+        Ordered.Text = $"Ordered: {line.Amount.ToString()}";
+        Refunded.Text = $"Refunded: {line.Refunded.ToString()}";
+
+        _line = line;
+        _onRefund = onRefund;
+    }
+
+    private readonly OrderLine _line;
+    private readonly Action<OrderLine, int> _onRefund;
+
+    private void ValidateInput(object sender, TextChangedEventArgs _)
+    {
+        var content = (sender as TextBox)?.Text;
+        if (!int.TryParse(content, out var num) || num > _line.Amount - _line.Refunded || num <= 0)
+        {
+            InvalidInput.Visibility = Visibility.Visible;
+            SaveRefund.IsEnabled = false;
+        }
+        else
+        {
+            InvalidInput.Visibility = Visibility.Hidden;
+            SaveRefund.IsEnabled = true;
+        }
+    }
+
+    private void RefundOrder(object _, RoutedEventArgs __)
+    {
+        if (int.TryParse(RefundAmount.Text, out var refunded) &&
+            refunded <= _line.Amount - _line.Refunded &&
+            refunded > 0)
+        {
+            _onRefund(_line, refunded);
+            Close();
+        }
+    }
+
+    private void Cancel(object _, RoutedEventArgs __)
+    {
+        Close();
+    }
+}
